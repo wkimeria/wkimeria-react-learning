a25c3f9a37755624e1454ac2f8db1cd9c505306e
William Kimeria <wkimeria@gmail.com>
2021-12-21 10:03:46 -0800

    Refactoring

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,18 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
 import './Expenses.css'
 
 function Expenses(props){
     const expenses = props.expenses;
 
     return (
-        <div className='expenses'>{expenses.map(
+        <Card className="expenses">{expenses.map(
             expense => <ExpenseItem
                 title={expense.title}
                 amount={expense.amount}
                 date={expense.date}/>
-        )}</div>
+        )}</Card>
     );
-
 }
 
 export default Expenses;
