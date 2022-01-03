2e5d1d5470ceb11ba4133efb1aa4bd0602bff7a9
William Kimeria <wkimeria@gmail.com>
2022-01-03 13:43:40 -0800

    Section 5: Assignment 3

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,20 +1,25 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpensesFilter"
 import Card from '../UI/Card';
 import './Expenses.css'
 
 const Expenses = (props) => {
-    const expenses = props.expenses;
+
+    const [expenses, _] = useState(props.expenses);
+    const [filteredExpenses, setExpenses] = useState(props.expenses);
 
     const handleYearFilterChange = (yearEntered) => {
-        // TODO: Do something with data
-        console.log("Called " + yearEntered);
+        setExpenses(_ => {
+            return expenses.filter( expense => { return expense.date.getFullYear().toString() === yearEntered;});
+        })
     }
 
     return (
         <div>
             <ExpenseFilter onYearFilterChange={handleYearFilterChange}/>
-            <Card className="expenses">{expenses.map(
+            <Card className="expenses">{filteredExpenses.map(
                 expense => <ExpenseItem
                     key={expense.id}
                     title={expense.title}
