2f37d29678dc3dd4b2a6957489884eebef3ccc50
wkimeria <wkimeria@gmail>
2022-01-03 14:32:06 -0800

    After Section 5: Conditional Return statements

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -6,11 +6,13 @@ import './ExpenseItem.css'
 const ExpenseItem = (props) => {
 
     return (
+        <li>
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description"><h2>{props.title}</h2></div>
             <div className="expense-item__price">{props.amount}</div>
-        </Card>);
+        </Card>
+        </li>);
 }
 
 export default ExpenseItem;
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpenseFilter from "./ExpensesFilter"
 import Card from '../UI/Card';
 import './Expenses.css'
@@ -16,22 +16,11 @@ const Expenses = (props) => {
         })
     }
 
-    let expensesContent = <p>No expenses found</p>
-    if(filteredExpenses.length > 0){
-        expensesContent = filteredExpenses.map(
-            expense => <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}/>
-        );
-    }
-
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter onYearFilterChange={handleYearFilterChange}/>
-                {expensesContent}
+                <ExpensesList items ={filteredExpenses}/>
             </Card>
         </div>
     );
